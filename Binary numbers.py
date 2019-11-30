e7d5630bf5098ae96313e7fb3959a69a59c564ca
Hrvoje <vhrvoje2@net.hr>
2019-11-30 17:44:33 +0100

    final cleanup

--- Binary numbers.py
+++ Binary numbers.py
@@ -5,25 +5,30 @@ import matplotlib.pyplot as plt
 unosAlgoritma = input("Unesite algoritam kriptiranja: ")
 
 #md5(), sha1(), sha224(), sha256(), sha384(), sha512(), blake2b(), blake2s(), sha3_224, sha3_256, sha3_384, sha3_512, shake_128, and shake_256.
-if unosAlgoritma == "md5":
-    velicinaListe = 128
-elif unosAlgoritma == "sha1":
-    velicinaListe = 160
-elif unosAlgoritma in ("sha224", "sha3_224"):
-    velicinaListe = 224
-elif unosAlgoritma in ("sha256", "blake2s", "sha3_256"):
-    velicinaListe = 256
-elif unosAlgoritma in ("sha384", "sha3_384"):
-    velicinaListe = 384
-elif unosAlgoritma in ("sha512", "sha3_512", "blake2b"):
-    velicinaListe = 512
+def velListe(algoritam):
+    if unosAlgoritma == "md5":
+        velicinaListe = 128
+    elif unosAlgoritma == "sha1":
+        velicinaListe = 160
+    elif unosAlgoritma in ("sha224", "sha3_224"):
+        velicinaListe = 224
+    elif unosAlgoritma in ("sha256", "blake2s", "sha3_256"):
+        velicinaListe = 256
+    elif unosAlgoritma in ("sha384", "sha3_384"):
+        velicinaListe = 384
+    elif unosAlgoritma in ("sha512", "sha3_512", "blake2b"):
+        velicinaListe = 512
+
+    return velicinaListe
 
 #lista za brojanje
 lista = []
+velicinaListe = velListe(unosAlgoritma)
+
 for x in range(velicinaListe):
     lista.append(0)
 
-for broj in range(2**8):
+for broj in range(2**16):
     #pretvaranje u binarni oblik
     binBroj = bin(int(broj))[2:]
 
@@ -38,12 +43,12 @@ for broj in range(2**8):
         if binarniHash[y] == "1":
             lista[y] += 1
 
-#maxValue = max(lista)/int(brojPonavljanja)
-#minValue = min(lista)/int(brojPonavljanja)
+maxValue = round(max(lista)/2**16, 2)
+minValue = round(min(lista)/2**16, 2)
 
 plt.figure(dpi=150)
 plt.title(unosAlgoritma.capitalize())
 plt.plot(lista, color='red', linewidth=0.5)
-#plt.ylabel(str(minValue) + "-" + str(maxValue))
-#plt.xlabel("Broj generiranih poruka: " + brojPonavljanja)
+plt.ylabel(str(minValue) + "-" + str(maxValue))
+plt.xlabel("Broj generiranih brojeva: " + str(2**16))
 plt.show()
